2564db0accf35edc77fb441e17fd4c5f671daebc
renier <reniermin88@protonmail.com>
2023-05-09 13:45:47 -0600

    added parameter validation

--- include/vCardProperty.h
+++ include/vCardProperty.h
@@ -17,32 +17,49 @@
 #define VC_TYPE_SEP_TOKEN '='
 
 #define VC_ADDRESS              "ADR"
-#define VC_AGENT                "AGENT"
+#define VC_ANNIVERSARY          "ANNIVERSARY"
 #define VC_BIRTHDAY             "BDAY"
+#define VC_BIRTHPLACE           "BIRTHPLACE"
+#define VC_CALURI               "CALURI"
+#define VC_CALADRURI            "CALADRURI"
 #define VC_CATEGORIES           "CATEGORIES"
-#define VC_CLASS                "CLASS"
-#define VC_DELIVERY_LABEL       "LABEL"
+#define VC_CONTACT_URI          "CONTACT-URI"
+#define VC_CLIENT_PID_MAP       "CLIENTPIDMAP"
+#define VC_DEATH_DATE           "DEATHDATE"
+#define VC_DEATH_PLACE          "DEATHPLACE"
 #define VC_EMAIL                "EMAIL"
+#define VC_EXPERTISE            "EXPERTISE"
 #define VC_FORMATTED_NAME       "FN"
+#define VC_FREEBUSY_URL         "FBURL"
+#define VC_GENDER               "GENDER"
 #define VC_GEOGRAPHIC_POSITION  "GEO"
+#define VC_HOBBY                "HOBBY"
+#define VC_IMPP                 "IMPP"
+#define VC_INTEREST             "INTEREST"
 #define VC_KEY                  "KEY"
+#define VC_KIND                 "KIND"
+#define VC_LANG                 "LANG"
 #define VC_LOGO                 "LOGO"
-#define VC_MAILER               "MAILER"
+#define VC_MEMBER               "MEMBER"
 #define VC_NAME                 "N"
 #define VC_NICKNAME             "NICKNAME"
 #define VC_NOTE                 "NOTE"
 #define VC_ORGANIZATION         "ORG"
+#define VC_ORG_DIRECTORY        "ORG-DIRECTORY"
 #define VC_PHOTO                "PHOTO"
 #define VC_PRODUCT_IDENTIFIER   "PRODID"
+#define VC_RELATED              "RELATED"
 #define VC_REVISION             "REV"
 #define VC_ROLE                 "ROLE"
-#define VC_SORT_STRING          "SORT-STRING"
 #define VC_SOUND                "SOUND"
+#define VC_SOURCE               "SOURCE"
 #define VC_TELEPHONE            "TEL"
 #define VC_TIME_ZONE            "TZ"
 #define VC_TITLE                "TITLE"
+#define VC_UID                  "UID"
 #define VC_URL                  "URL"
 #define VC_VERSION              "VERSION"
+#define VC_XML                  "XML"
 
 /**
  * @brief vCard line property
@@ -73,9 +90,8 @@ public:
     vCardProperty() {}
     vCardProperty(const std::string& group, const std::string& name, const std::string& value);
     vCardProperty(const std::string& name, const std::string& value);
-    vCardProperty(const std::string& name, const std::vector<std::string>& values): m_name(name), m_values(values) {}
-    vCardProperty(const std::string& name, const std::vector<std::string>& values, const vCardParamMap params):
-            m_name(name), m_values(values), m_params(params) {}
+    vCardProperty(const std::string& name, const std::vector<std::string>& values);
+    vCardProperty(const std::string& name, const std::vector<std::string>& values, const vCardParamMap params);
     ~vCardProperty() {}
 
     std::string& getName() { return m_name; }
@@ -92,6 +108,9 @@ public:
     std::vector<std::string>& values() { return m_values; }
 
     void setParams(vCardParamMap params);
+    vCardParamMap getParams() { return m_params; }
+
+    [[deprecated("Method bypasses validation")]]
     vCardParamMap& params() { return m_params; }
 
     /**
@@ -117,6 +136,8 @@ protected:
     std::string m_name;
     std::vector<std::string> m_values;
     vCardParamMap m_params;
+
+    bool isValidParamMap(vCardParamMap params);
 };
 
 #endif //VCARD_VCARDPROPERTY_H
--- src/vCardProperty.cpp
+++ src/vCardProperty.cpp
@@ -29,9 +29,25 @@ vCardProperty::vCardProperty(const std::string& group, const std::string& name, 
     m_group = group;
     m_values = split(value, VC_SEPARATOR_TOKEN, true);
 }
+vCardProperty::vCardProperty(const std::string& name, const std::vector<std::string>& values) {
+    m_name = name;
+    m_values = values;
+}
+
+vCardProperty::vCardProperty(const std::string& name, const std::vector<std::string>& values, const vCardParamMap params) {
+    m_name = name;
+    m_values = values;
+    if (!isValidParamMap(params)) {
+        throw std::invalid_argument("invalid param map");
+    }
+    m_params = params;
+}
 
 void vCardProperty::setParams(vCardParamMap params)
 {
+    if (!isValidParamMap(params)) {
+        throw std::invalid_argument("invalid param map");
+    }
     this->m_params = params;
 }
 
@@ -52,6 +68,9 @@ std::string vCardProperty::getValue()
 
 vCardProperty & vCardProperty::operator << (const vCardParamMap &p)
 {
+    if (!isValidParamMap(p)) {
+        throw std::invalid_argument("invalid param map");
+    }
     this->setParams(p);
     return *this;
 }
@@ -69,7 +88,10 @@ vCardProperty vCardProperty::createAddress(const std::string& street, const std:
     values.push_back(postal_code);
     values.push_back(country);
 
-    return vCardProperty(VC_ADDRESS, values, params);
+    vCardProperty cardProperty = vCardProperty(VC_ADDRESS, values);
+    cardProperty.setParams(params);
+
+    return cardProperty;
 }
 
 vCardProperty vCardProperty::createName(const std::string& firstname, const std::string& lastname,
@@ -83,7 +105,10 @@ vCardProperty vCardProperty::createName(const std::string& firstname, const std:
     values.push_back(prefix);
     values.push_back(suffix);
 
-    return vCardProperty(VC_NAME, values, params);
+    vCardProperty cardProperty = vCardProperty(VC_NAME, values);
+    cardProperty.setParams(params);
+
+    return cardProperty;
 }
 
 vCardProperty vCardProperty::createOrganization(const std::string& name, const std::vector<std::string>& levels,
@@ -93,7 +118,10 @@ vCardProperty vCardProperty::createOrganization(const std::string& name, const s
     values.push_back(name);
     values.insert(values.end(), levels.begin(), levels.end());
 
-    return vCardProperty(VC_ORGANIZATION, values, params);
+    vCardProperty cardProperty = vCardProperty(VC_ORGANIZATION, values);
+    cardProperty.setParams(params);
+
+    return cardProperty;
 }
 
 vCardProperty vCardProperty::createBirthday(const int year, const int month, const int day)
@@ -101,5 +129,262 @@ vCardProperty vCardProperty::createBirthday(const int year, const int month, con
     char buffer[15];
     sprintf(buffer, "%4d-%2d-%2d", year, month, day);
     return vCardProperty(VC_BIRTHDAY, buffer);
+}
+
+bool vCardProperty::isValidParamMap(vCardParamMap params) {
+    bool isValid = true;
+
+    std::vector<std::string> disallowed;
+
+    if (m_name == VC_ADDRESS) {
+        disallowed = {
+                "MEDIATYPE", "CALSCALE", "SORT-AS", "LEVEL"
+        };
+    }
+    else if (m_name == VC_ANNIVERSARY) {
+        disallowed = {
+                "LANGUAGE", "PREF", "PID", "TYPE", "MEDIATYPE", "SORT-AS",
+                "GEO", "TZ", "LABEL", "CC", "INDEX", "LEVEL"
+        };
+    }
+    else if (m_name == VC_BIRTHDAY) {
+        disallowed = {
+                "PREF", "PID", "TYPE", "MEDIATYPE", "SORT-AS", "GEO", "TZ",
+                "LABEL", "CC", "INDEX", "LEVEL"
+        };
+    }
+    else if (m_name == VC_BIRTHPLACE) {
+        disallowed = {
+                "PREF", "PID", "TYPE", "MEDIATYPE", "CALSCALE", "SORT-AS",
+                "GEO", "TZ", "LABEL", "CC", "INDEX", "LEVEL"
+        };
+    }
+    else if (m_name == VC_CALURI || m_name == VC_CALADRURI) {
+        disallowed = {
+                "LANGUAGE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL",
+                "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_CATEGORIES) {
+        disallowed = {
+                "LANGUAGE", "MEDIATYPE", "CALSCALE", "SORT-AS", "GEO",
+                "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_CONTACT_URI) {
+        disallowed = {
+                "LANGUAGE", "ALTID", "PID", "TYPE", "MEDIATYPE", "CALSCALE",
+                "SORT-AS", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_CLIENT_PID_MAP) {
+        disallowed = {
+                "LANGUAGE", "VALUE", "PREF", "ALTID", "PID", "TYPE", "MEDIATYPE",
+                "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "INDEX", "LEVEL"
+        };
+    }
+    else if (m_name == VC_DEATH_DATE) {
+        disallowed = {
+                "PREF", "PID", "TYPE", "MEDIATYPE", "SORT-AS", "GEO", "TZ", "LABEL",
+                "CC", "INDEX", "LEVEL"
+        };
+    }
+    else if (m_name == VC_DEATH_PLACE) {
+        disallowed = {
+                "PREF", "PID", "TYPE", "MEDIATYPE", "SORT-AS", "GEO", "TZ", "LABEL",
+                "CC", "INDEX", "LEVEL", "CALSCALE"
+        };
+    }
+    else if (m_name == VC_EMAIL){
+        disallowed = {
+                "LANGUAGE", "MEDIATYPE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL",
+                "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_EXPERTISE) {
+        disallowed = {
+                "VALUE", "PID", "MEDIATYPE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL",
+                "CC"
+        };
+    }
+    else if (m_name == VC_FORMATTED_NAME) {
+        disallowed = {
+                "TYPE", "MEDIATYPE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC",
+                "LEVEL"
+        };
+    }
+    else if (m_name == VC_FREEBUSY_URL) {
+        disallowed = {
+                "LANGUAGE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_GENDER) {
+        disallowed = {
+                "LANGUAGE", "PREF", "ALTID", "PID", "TYPE", "MEDIATYPE", "CALSCALE",
+                "SORT-AS", "GEO", "TZ", "LABEL", "CC", "INDEX", "LEVEL"
+        };
+    }
+    else if (m_name == VC_GEOGRAPHIC_POSITION) {
+        disallowed = {
+                "LANGUAGE", "CALSCALE", "SORT-AS", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_HOBBY) {
+        disallowed = {
+                "VALUE", "PID", "MEDIATYPE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL",
+                "CC"
+        };
+    }
+    else if (m_name == VC_IMPP) {
+        disallowed = {
+                "LANGUAGE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_INTEREST) {
+        disallowed = {
+                "VALUE", "PID", "TYPE", "MEDIATYPE", "CALSCALE", "SORT-AS", "GEO", "TZ",
+                "LABEL", "CC"
+        };
+    }
+    else if (m_name == VC_KEY) {
+        disallowed = {
+                "LANGUAGE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_KIND) {
+        disallowed = {
+            "LANGUAGE", "PREF", "ALTID", "PID", "TYPE", "MEDIATYPE", "CALSCALE",
+            "SORT-AS", "GEO", "TZ", "LABEL", "CC", "INDEX", "LEVEL"
+        };
+    }
+    else if (m_name == VC_LANG) {
+        disallowed = {
+                "LANGUAGE", "MEDIATYPE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL",
+                "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_LOGO) {
+        disallowed = {
+                "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "INDEX", "LEVEL"
+        };
+    }
+    else if (m_name == VC_MEMBER) {
+        disallowed = {
+                "LANGUAGE", "TYPE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC",
+                "LEVEL"
+        };
+    }
+    else if (m_name == VC_NAME) {
+        disallowed = {
+                "PREF", "PID", "TYPE", "MEDIATYPE", "CALSCALE", "SORT-AS", "GEO", "TZ",
+                "LABEL", "CC", "INDEX", "LEVEL"
+        };
+    }
+    else if (m_name == VC_NICKNAME) {
+        disallowed = {
+                "MEDIATYPE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_NOTE) {
+        disallowed = {
+                "MEDIATYPE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_ORGANIZATION) {
+        disallowed = {
+                "MEDIATYPE", "CALSCALE", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_ORG_DIRECTORY) {
+        disallowed = {
+                "VALUE", "MEDIATYPE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC",
+                "LEVEL"
+        };
+    }
+    else if (m_name == VC_PHOTO) {
+        disallowed = {
+                "LANGUAGE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_PRODUCT_IDENTIFIER) {
+        disallowed = {
+                "LANGUAGE", "PREF", "ALTID", "PID", "TYPE", "MEDIATYPE", "CALSCALE",
+                "SORT-AS", "GEO", "TZ", "LABEL", "CC", "INDEX", "LEVEL"
+        };
+    }
+    else if (m_name == VC_RELATED) {
+        disallowed = {
+                "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_REVISION) {
+        disallowed = {
+                "LANGUAGE", "PREF", "ALTID", "PID", "TYPE", "MEDIATYPE", "CALSCALE",
+                "SORT-AS", "GEO", "TZ", "LABEL", "CC", "INDEX", "LEVEL"
+        };
+    }
+    else if (m_name == VC_ROLE) {
+        disallowed = {
+                "MEDIATYPE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "INDEX",
+                "LEVEL"
+        };
+    }
+    else if (m_name == VC_SOUND) {
+        disallowed = {
+                "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_SOURCE) {
+        disallowed = {
+                "LANGUAGE", "PID", "TYPE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL",
+                "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_TELEPHONE) {
+        disallowed = {
+                "LANGUAGE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_TIME_ZONE) {
+        disallowed = {
+                "LANGUAGE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_TITLE) {
+        disallowed = {
+                "MEDIATYPE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_UID) {
+        disallowed = {
+                "LANGUAGE", "PREF", "ALTID", "PID", "TYPE", "MEDIATYPE", "CALSCALE",
+                "SORT-AS", "GEO", "TZ", "LABEL", "CC", "INDEX", "LEVEL"
+        };
+    }
+    else if (m_name == VC_URL) {
+        disallowed = {
+                "LANGUAGE", "CALSCALE", "SORT-AS", "GEO", "TZ", "LABEL", "CC", "LEVEL"
+        };
+    }
+    else if (m_name == VC_VERSION) {
+        disallowed = {
+                "LANGUAGE", "PREF", "ALTID", "PID", "TYPE", "MEDIATYPE", "CALSCALE",
+                "SORT-AS", "GEO", "TZ", "LABEL", "CC", "INDEX", "LEVEL"
+        };
+    }
+    else if (m_name == VC_XML) {
+        disallowed = {
+                "LANGUAGE", "PREF", "PID", "TYPE", "MEDIATYPE", "CALSCALE", "SORT-AS",
+                "GEO", "TZ", "LABEL", "CC", "INDEX", "LEVEL"
+        };
+    }
+
+    for (const auto& s : disallowed) {
+        if (params.contains(s)) {
+            isValid = false;
+        }
+    }
+
+    return isValid;
 }
 
