25e6b043160e8a3ec979f22f5fefc4359ad20d23
badmister <support@imister.kz>
2025-01-10 16:53:58 +0500

    Minor Update (Patch) What has changed?
        PARSER.
    
    What has been done?
    
        Fixed minor crashes during parsing. This code improvement was aimed at addressing certain areas in the code, according to my reports (which include about 100 VCF data files from various version categories).
    
    CONCLUSION:
    
    The minor code includes these fixes, allowing for continued data import even in the case of exceptions, which, in turn, facilitates successful imports.

--- src/text_io.cpp
+++ src/text_io.cpp
@@ -20,7 +20,7 @@
 #include "text_io.h"
 #include "utils.h"
 
-std::istream* safeGetline(std::istream *is, std::string& t)
+std::istream *safeGetline(std::istream *is, std::string &t)
 {
     t.clear();
 
@@ -31,18 +31,22 @@ std::istream* safeGetline(std::istream *is, std::string& t)
     // such as thread synchronization and updating the stream state.
 
     std::istream::sentry se(*is, true);
-    std::streambuf* sb = is->rdbuf();
+    std::streambuf *sb = is->rdbuf();
 
-    for(;;) {
+    for(;;)
+    {
         int c = sb->sbumpc();
-        switch (c) {
+        switch(c)
+        {
             case '\n':
                 return is;
             case '\r':
-                if(sb->sgetc() == '\n'){
+                if(sb->sgetc() == '\n')
+                {
                     sb->sbumpc();
                     // special case for vCard folding lines
-                    if(sb->sgetc() == ' ' || sb->sgetc() == '\t'){
+                    if(sb->sgetc() == ' ' || sb->sgetc() == '\t' || sb->sgetc() == '=' || sb->sgetc() == ';')
+                    {
                         sb->sbumpc();
                         continue;
                     }
@@ -55,7 +59,7 @@ std::istream* safeGetline(std::istream *is, std::string& t)
                     is->setstate(std::ios::eofbit);
                 return is;
             default:
-                t += (char)c;
+                t += (char) c;
         }
     }
 }
@@ -63,21 +67,25 @@ std::istream* safeGetline(std::istream *is, std::string& t)
 vCardParamMap TextReader::parseParams(std::string input)
 {
     vCardParamMap params;
-    int i=0, start_pos=0;
-    while(i < input.size()){
+    int i = 0, start_pos = 0;
+    while(i < input.size())
+    {
         // get params name
         while(input[i] != '=' && i < input.size())
             i++;
 
-        std::string name = input.substr(start_pos, i-start_pos);
+        std::string name = input.substr(start_pos, i - start_pos);
         i++; // skip '='
         start_pos = i;
 
         // get params values
-        while(true){
-            while(input[i] != ';' && input[i] != ',' && i < input.size()){
-                if(input[i] == '\"'){ // read everything inside ""
-                    i++; //skip first '"'
+        while(true)
+        {
+            while(i < input.size() && input[i] != ';' && input[i] != ',')
+            {
+                if(input[i] == '\"')
+                {        // read everything inside ""
+                    i++; // skip first '"'
                     start_pos = i;
                     while(input[i] != '\"' && i < input.size())
                         i++;
@@ -86,28 +94,31 @@ vCardParamMap TextReader::parseParams(std::string input)
                         throw std::runtime_error("Parsing error: not closed DQUOTE");
 
                     break;
-
-                } else {
+                }
+                else
+                {
                     i++;
                 }
             }
 
-            std::string value = input.substr(start_pos, i-start_pos);
-            std::transform(name.begin(), name.end(), name.begin(),
-                           [](unsigned char c){ return std::toupper(c); });
-            if(name == "TYPE" || name == "SORT-AS"){
+            std::string value = input.substr(start_pos, i - start_pos);
+
+            std::transform(name.begin(), name.end(), name.begin(), [](unsigned char c) { return std::toupper(c); });
+            if(name == "TYPE" || name == "SORT-AS")
+            {
                 std::vector<std::string> p = split(value, ',');
-                for(auto& s: p)
+                for(auto &s : p)
                     params.addParam(name, s);
-            } else
+            }
+            else
                 params.addParam(name, value);
 
             if(input[i] == '\"')
-                i++; //skip last '"'
-            i++; //skip ';' or ','
+                i++; // skip last '"'
+            i++;     // skip ';' or ','
             start_pos = i;
 
-            if(input[i-1] == ';' || i >= input.size())
+            if(input[i - 1] == ';' || i >= input.size())
                 break;
         }
     }
@@ -117,17 +128,18 @@ vCardParamMap TextReader::parseParams(std::string input)
 
 vCardProperty TextReader::parseProperty(std::string line)
 {
-    //find first nonquoted ':', first ';' and first '.'
-    int i=0, colon_pos=-1, first_semicln_pos=-1, first_point_pos=-1 ;
+    // find first nonquoted ':', first ';' and first '.'
+    int i = 0, colon_pos = -1, first_semicln_pos = -1, first_point_pos = -1;
     bool quoted = false;
-    while(i<line.size()){
+    while(i < line.size())
+    {
         if(line[i] == VC_ASSIGNMENT_TOKEN && !quoted)
             break;
 
-        if(line[i] == ';' && first_semicln_pos<0) // first ';' could not be quoted
+        if(line[i] == ';' && first_semicln_pos < 0) // first ';' could not be quoted
             first_semicln_pos = i;
 
-        if(line[i] == '.' && first_point_pos<0)
+        if(line[i] == '.' && first_point_pos < 0)
             first_point_pos = i;
 
         if(line[i] == '\"')
@@ -142,28 +154,61 @@ vCardProperty TextReader::parseProperty(std::string line)
 
     std::string group, name;
     vCardParamMap params;
-    if(first_semicln_pos > 0 && first_semicln_pos < colon_pos){ // we have parameters in property
-        if(first_point_pos > 0 && first_point_pos < first_semicln_pos){
+    if(first_semicln_pos > 0 && first_semicln_pos < colon_pos)
+    { // we have parameters in property
+        if(first_point_pos > 0 && first_point_pos < first_semicln_pos)
+        {
             group = line.substr(0, first_point_pos);
-            name = line.substr(first_point_pos+1, first_semicln_pos - first_point_pos - 1);
-        } else {
+            name = line.substr(first_point_pos + 1, first_semicln_pos - first_point_pos - 1);
+        }
+        else
+        {
             name = line.substr(0, first_semicln_pos);
         }
 
-        if (name != VC_VERSION) {
-            std::string params_string = line.substr(first_semicln_pos+1, colon_pos - first_semicln_pos - 1);
+        if(name != VC_VERSION)
+        {
+            std::string params_string = line.substr(first_semicln_pos + 1, colon_pos - first_semicln_pos - 1);
+            // Issue: Fix error on CELL or other single type.
+            if(params_string.find("=") == std::string::npos)
+            {
+                constexpr auto TYPE_STR = "TYPE=";
+                std::string validate {};
+                i = 0;
+                first_semicln_pos = 0;
+                params_string+=';';
+                while(i < params_string.length())
+                {
+                    if(params_string[i] == ';')
+                    {
+                        validate += TYPE_STR;
+                        validate += params_string.substr(first_semicln_pos, i - first_semicln_pos);
+                        first_semicln_pos=i+1;
+                        validate += ';'; // Next
+                    }
+                    ++i;
+                }
+
+                params_string = validate;
+            }
+
             params = TextReader::parseParams(params_string);
         }
-    } else { // we do not have parameters
-        if(first_point_pos > 0 && first_point_pos < colon_pos){
+    }
+    else
+    { // we do not have parameters
+        if(first_point_pos > 0 && first_point_pos < colon_pos)
+        {
             group = line.substr(0, first_point_pos);
-            name = line.substr(first_point_pos+1, colon_pos - first_point_pos);
-        } else {
+            name = line.substr(first_point_pos + 1, colon_pos - first_point_pos);
+        }
+        else
+        {
             name = line.substr(0, colon_pos);
         }
     }
 
-    std::string value = line.substr(colon_pos+1);
+    std::string value = line.substr(colon_pos + 1);
     vCardProperty prop(group, name, value);
     prop.setParams(params);
     return prop;
@@ -174,22 +219,28 @@ std::vector<vCard> TextReader::parseCards()
     std::vector<vCard> vcards;
     vCard current;
     bool started = false;
-
+    bool trySkipContent = false;
     std::string line;
-    while(!safeGetline(m_is, line)->eof()){
-        if ((line == VC_BEGIN_TOKEN) && !started) {
+    while(!safeGetline(m_is, line)->eof())
+    {
+        if((line == VC_BEGIN_TOKEN) && !started)
+        {
             started = true;
-        } else if ((line == VC_END_TOKEN) && started) {
+        }
+        else if((line == VC_END_TOKEN) && started)
+        {
             vcards.push_back(current);
-            
+            trySkipContent = false;
+            started = false;
             // Empty the current card
             current = vCard();
-            
-            started = false;
-        } else if ((line.find("VERSION") != std::string::npos) && started) {
+        }
+        else if((line.find("VERSION") != std::string::npos) && started)
+        {
             size_t pos = line.find(":");
-            if(pos != std::string::npos){
-                std::string version = line.substr(pos+1);
+            if(pos != std::string::npos)
+            {
+                std::string version = line.substr(pos + 1);
                 trim(version);
                 if(version == "3.0")
                     current.setVersion(VC_VER_3_0);
@@ -198,34 +249,56 @@ std::vector<vCard> TextReader::parseCards()
             }
 
             continue;
-        } else if (started) {
-            vCardProperty prop = TextReader::parseProperty(line);
-            current.addProperty(prop);
+        }
+        else if(started)
+        {
+            // Check data (PHOTO), (SOUND)
+            if(line.find("X-CUSTOM") != std::string::npos || line.find("PHOTO") != std::string::npos || line.find("SOUND") != std::string::npos)
+            {
+                trySkipContent = true;
+                continue;
+            }
+            if(line.empty() || trySkipContent && line.at(0) == ' ')
+            {
+                continue;
+            }
+            try
+            {
+                vCardProperty prop = TextReader::parseProperty(line);
+                current.addProperty(prop);
+            }
+            catch(std::exception)
+            {
+                continue;
+            }
+
+            trySkipContent = false;
         }
     }
 
     return vcards;
 }
 
-TextReader & TextReader::operator >> (vCard & vCard)
+TextReader &TextReader::operator>>(vCard &vCard)
 {
     return *this;
 }
 
-TextReader & TextReader::operator >> (std::vector<vCard> & cards)
+TextReader &TextReader::operator>>(std::vector<vCard> &cards)
 {
     return *this;
 }
 
 //============================================================================
 
-TextWriter & TextWriter::operator << (vCard & vCard)
+TextWriter &TextWriter::operator<<(vCard &vCard)
 {
     *m_os << VC_BEGIN_TOKEN << VC_END_LINE_TOKEN;
     *m_os << "VERSION:" << vCard.getVersionStr() << VC_END_LINE_TOKEN;
 
-    for(auto it = vCard.properties().begin(); it != vCard.properties().end(); ++it){
-        //TODO make line folding (split lines with length > 75)
+    for(auto it = vCard.properties().begin(); it != vCard.properties().end(); ++it)
+    {
+        // TODO make line folding (split lines with length > 75)
         *this << *it;
         *m_os << VC_END_LINE_TOKEN;
     }
@@ -234,23 +307,25 @@ TextWriter & TextWriter::operator << (vCard & vCard)
     return *this;
 }
 
-TextWriter & TextWriter::operator << (std::vector<vCard> & cards)
+TextWriter &TextWriter::operator<<(std::vector<vCard> &cards)
 {
-    for(auto card: cards){
+    for(auto card : cards)
+    {
         *this << card;
     }
 
     return *this;
 }
 
-TextWriter & TextWriter::operator << (vCardProperty & p)
+TextWriter &TextWriter::operator<<(vCardProperty &p)
 {
     if(!p.getGroup().empty())
         *m_os << p.getGroup() << '.';
 
     *m_os << p.getName();
 
-    if(p.params().size() > 0){
+    if(p.params().size() > 0)
+    {
         *m_os << ';';
         *this << p.params();
     }
@@ -258,7 +333,8 @@ TextWriter & TextWriter::operator << (vCardProperty & p)
     *m_os << ':';
 
     auto it = p.values().begin();
-    while(it != p.values().end()){
+    while(it != p.values().end())
+    {
         *m_os << *it;
 
         it++;
@@ -270,10 +346,11 @@ TextWriter & TextWriter::operator << (vCardProperty & p)
     return *this;
 }
 
-TextWriter & TextWriter::operator << (vCardParamMap & p)
+TextWriter &TextWriter::operator<<(vCardParamMap &p)
 {
     auto it = p.begin();
-    while(it != p.end()){
+    while(it != p.end())
+    {
         *m_os << it->first << "=";
         size_t found = it->second.find_first_of(":;,");
         if(found == std::string::npos)
